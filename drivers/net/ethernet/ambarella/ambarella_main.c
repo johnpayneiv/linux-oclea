8ce2c6fd39a7dfc882290c039eca6106aae5fd65
John Payne <jpayne@thecrestfactor.com>
2024-06-23 16:47:51 -0400

    Unwinde a change to see if it is nesscisary post getting the fpga phy setup.

--- drivers/net/ethernet/ambarella/ambarella_main.c
+++ drivers/net/ethernet/ambarella/ambarella_main.c
@@ -1573,8 +1573,8 @@ static int ambeth_stop(struct net_device *ndev)
 	netif_carrier_off(ndev);
 	phy_stop(lp->phydev);
 	phy_disconnect(lp->phydev);
-	if (of_phy_is_fixed_link(ndev->dev.parent->of_node)) 
-		of_phy_deregister_fixed_link(ndev->dev.parent->of_node);	
+	// if (of_phy_is_fixed_link(ndev->dev.parent->of_node)) 
+	// 	of_phy_deregister_fixed_link(ndev->dev.parent->of_node);	
 	ambeth_phy_stop(lp);
 	ambeth_stop_hw(ndev);
 
@@ -2634,6 +2634,7 @@ static int ambeth_drv_probe(struct platform_device *pdev)
 	int ret_val = 0;
 	//bool fixed_link = false;
 
+	printk("eth: probe\n");
 	ndev = alloc_etherdev(sizeof(struct ambeth_info));
 	if (ndev == NULL) {
 		dev_err(&pdev->dev, "alloc_etherdev fail.\n");
@@ -2881,8 +2882,8 @@ static int ambeth_drv_suspend(struct platform_device *pdev, pm_message_t state)
 	if (lp->phydev) {
 		phy_stop(lp->phydev);
 		phy_disconnect(lp->phydev);
-		if (of_phy_is_fixed_link(ndev->dev.parent->of_node)) 
-			of_phy_deregister_fixed_link(ndev->dev.parent->of_node);			
+		// if (of_phy_is_fixed_link(ndev->dev.parent->of_node)) 
+		// 	of_phy_deregister_fixed_link(ndev->dev.parent->of_node);			
 	}
 
 	napi_disable(&lp->napi);
